75b73d1bfe12011f2d942cb4a37e67304f91f058
LucasIde <lucaside000@gmail.com>
2023-05-31 16:40:44 +0200

    plus rapide

--- set_docker.sh
+++ set_docker.sh
@@ -1,7 +1,8 @@
 sudo apt-get install curl -y
 
-curl -fsSL https://get.docker.com -o get-docker.sh
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo apt-get install docker-ce docker-compose make -y
 
-rm get-docker.sh
+sudo rm get-docker.sh
 sudo reboot
