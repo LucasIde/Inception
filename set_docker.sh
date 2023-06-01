5fd9038b6b937e4168c26c997e5ddab915f943be
LucasIde <lucaside000@gmail.com>
2023-06-01 18:16:13 +0200

    plus rapide

--- set_docker.sh
+++ set_docker.sh
@@ -1,8 +1,19 @@
-sudo apt-get install curl -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo apt-get install docker-ce docker-compose make -y
+sudo apt-get install curl make git -y
+sudo apt-get install ca-certificates gnupg lsb-release -y
 
-sudo rm get-docker.sh
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin -y
+
+sudo adduser $USER docker
+
 sudo reboot
